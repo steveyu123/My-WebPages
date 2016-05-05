fd5491d71fe00d31bf23e2f5a66d9fa66931e9ad
Steve <steveyu123@users.noreply.github.com>
2016-05-04 23:59:09 -0700

    Added files via upload

---
+++ simpleTodoList/list.js
@@ -0,0 +1,23 @@
+var todos = ["Buy food"];
+
+var input = prompt("What would you like to do?");
+
+
+
+while(input !== "quit") {
+
+	if(input === "list"){
+	console.log(todos);
+
+} else if(input === "new"){
+
+
+	var newTodo = prompt("Enter new todo");
+
+	todos.push(newTodo);
+}
+
+input = prompt("What would you like to do?");
+}
+
+console.log("Ok, you are done!");
